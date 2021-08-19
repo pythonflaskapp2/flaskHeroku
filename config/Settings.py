f3e2d3c8cc83b6442d7b1ad980dc9d6f39fe3b2a
pythonflaskapp2 <89172253+pythonflaskapp2@users.noreply.github.com>
2021-08-19 15:54:52 +0800

    Update Settings.py

--- config/Settings.py
+++ config/Settings.py
@@ -1,2 +1,6 @@
 class Settings:
     secretKey = "321897kdbmgj321djnbug#@4rhxcjk6(!d)"
+    host = os.environ['HOST']
+    database = os.environ['DATABASE']
+    user = os.environ['USERNAME']
+    password = os.environ['PASSWORD']
